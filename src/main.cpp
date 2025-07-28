d8e145f0f724d9663448116e610e8ac9496e1838
svnnikita <svnnikitos@gmail.com>
2025-07-28 11:36:27 +0300

    Мигание светодиодом с настройкой частоты

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,40 @@
-#include <libopencm3/stm32/rcc.h> //rcc.h - reset and clock control
-#include <libopencm3/stm32/gpio.h> //inputs outputs
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
-int main() {
-    rcc_periph_clock_enable(RCC_GPIOB);
+static void clock_setup(void)
+{
+	/* Set STM32 to 64 MHz. */
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
-    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO2);
+	/* Enable alternate function peripheral clock. */
+	//rcc_periph_clock_enable(RCC_AFIO);
 
-    while (true) {
+	/* Enable GPIOB clock. */
+	rcc_periph_clock_enable(RCC_GPIOB);
+}
 
-        gpio_toggle(GPIOB, GPIO2);
-        for(volatile uint32_t i =0; i < 500'000; i +=2);
+static void gpio_setup(void)
+{
+	
+	/* Set GPIO4 and 5 (in GPIO port B) to 'output push-pull'. */
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO2);
+	
+}
 
-    }
+int main(void)
+{
+	volatile int i;
+
+	clock_setup();
+	gpio_setup();
+
+	/* Blink the LEDs on the board. */
+	while (1) {
+		gpio_toggle(GPIOB, GPIO2);	/* LED on/off */
+		for (i = 0; i < 4000000; i++) /* Wait a bit. */
+			__asm__("nop");
+	}
+
+	return 0;
 }
